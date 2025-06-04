92a4f2689373940ccefb82736ea5f1ef29ef7873
Judit M <juditmitjans@gmail.com>
2025-06-04 19:47:49 +0200

    añadir protección de rutas

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Auth from '../views/Auth.vue'
 import Dashboard from '../views/Dashboard.vue'
+import { useUserStore } from '@/stores/user'
+import { storeToRefs } from 'pinia'
 
 const routes = [
   { path: '/auth', name: 'Auth', component: Auth },
-  { path: '/', name: 'Dashboard', component: Dashboard }
+  { path: '/', name: 'Dashboard', component: Dashboard, meta: { requiresAuth: true } }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const userStore = useUserStore()
+  const { user } = storeToRefs(userStore)
+
+  if (to.meta.requiresAuth && !user.value) {
+    next('/auth')
+  } else {
+    next()
+  }
 })
 
 export default router
